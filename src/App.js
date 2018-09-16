2cea9f5fd20a5139bd957b04ad32ce97e0805e47
John Joseph <atiquajohn@gmail.com>
2018-09-16 15:22:12 +0700

    added travis-ci config for deployment

--- src/App.js
+++ src/App.js
@@ -11,7 +11,7 @@ class App extends Component {
           <h1 className="App-title">Welcome to React-app</h1>
         </header>
         <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
+          Hey there we go! yahoo!
         </p>
       </div>
     );
